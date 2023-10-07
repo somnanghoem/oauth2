da656264b5949971181a5bc09a9391d8a3f6bfca
Hoem Somnang <hoemsomnangruppa4@gmail.com>
2023-10-07 10:32:46 +0700

    add Oauth2 default config

--- src/main/java/com/resource/oauth2/security/SecurityConfiguration.java
+++ src/main/java/com/resource/oauth2/security/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests( (authorize)  -> authorize.requestMatchers( "/api/v1/token/**").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                //.oauth2ResourceServer( (oauth2) -> oauth2.jwt((jwt)-> jwt.jwtAuthenticationConverter( jwtUserConverter ) ) )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())) // Default Config
                 .sessionManagement( (session)-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) );
                 /*.exceptionHandling((exception)-> exception
                         .authenticationEntryPoint( new BearerTokenAuthenticationEntryPoint())
